cff3c2c9a5c43b15ab77d9d86602189ec86970f0
Daniel Stahl <danny.stahl@gmail.com>
2020-05-26 08:35:52 -0700

    Initializing lab 8

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LAB_8_Stahl_Daniel
+//
+//  Created by Daniel Stahl on 5/26/20.
+//  Copyright © 2020 Daniel Stahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
